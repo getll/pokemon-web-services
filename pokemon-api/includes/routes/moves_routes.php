a38a3916fa9075f0fb1f339d1c35c44f57f229f8
Denmar Ermitano <ermitano.den@gmail.com>
2022-12-03 00:24:32 -0500

    added example for put route validation

--- pokemon-api/includes/routes/moves_routes.php
+++ pokemon-api/includes/routes/moves_routes.php
@@ -98,7 +98,8 @@ function handleCreateMove(Request $request, Response $response, array $args) {
     if (isset($requested_format[0]) && $requested_format[0] === APP_MEDIA_TYPE_JSON) {
         
         foreach ($parsed_body as $single_move) {
-            if (validateMove($single_move)) {
+            // validate move and check if it does not already exist since name is unique
+            if (validateMove($single_move) && isset($single_move["name"]) && !$moves_model->getMoveByName($single_move["name"])) {
                 // going through each field in a row
                 $move_name = $single_move["name"];
                 $move_desc = $single_move["description"];
@@ -150,48 +151,91 @@ function handleCreateMove(Request $request, Response $response, array $args) {
 
 function handleUpdateMove(Request $request, Response $response, array $args) {
     $response_code = HTTP_CREATED;
-    $moves = "";
+    
+    // ---------------------------------------------------------------------------------------------------------------------------------------
+    // new response for validation
+    // $moves = "";
+    $valid_rows = array();
+    $rows_not_added = 0;
     
     $moves_model = new MovesModel();
     $parsed_body = $request->getParsedBody();
+    
+    // ---------------------------------------------------------------------------------------------------------------------------------------
+    // validation for $parsed body / request body
+    // checking for request body
+    if (!$parsed_body || !(is_array($parsed_body) && array_is_list($parsed_body)) || empty($parsed_body)) {
+        $response_code = HTTP_BAD_REQUEST;
+        $response_data = json_encode(getErrorBadRequest("Missing or badly formatted request body."));
+        $response->getBody()->write($response_data);
+        return $response->withStatus($response_code);
+    }
     
     $requested_format = $request->getHeader('Accept');
     if (isset($requested_format[0]) && $requested_format[0] === APP_MEDIA_TYPE_JSON) {
         
         foreach ($parsed_body as $single_move) {
-            // going through each field in a row
-            $move_id = $single_move["move_id"];
-            $move_name = $single_move["name"];
-            $move_desc = $single_move["description"];
-            $move_category = $single_move["category"];
-            $move_power = isset($single_move["power"]) ? $single_move["power"] : null;
-            $move_accuracy = isset($single_move["accuracy"]) ? $single_move["accuracy"] : null;
-            $move_power_points = $single_move["power_points"];
-            $move_type = $single_move["type"];
-            $move_secondary_effect = ($single_move["has_secondary_effect"]) ? 1 : 0;
+            // ---------------------------------------------------------------------------------------------------------------------------------------
+            // validation added in foreach loop, checking the $single move row using validate function
+            // validation excludes if the id of the row isset, to add in validation pls
+            // id can also checked if it exists in db, but still technically valid if it doesnt. query will just not happen ---> it is this ($moves_model->getMoveById($single_move["move_id"])
+            if (validateMove($single_move) && isset($single_move["move_id"]) && $moves_model->getMoveById($single_move["move_id"])) {
+                // going through each field in a row
+                $move_id = $single_move["move_id"];
+                $move_name = $single_move["name"];
+                $move_desc = $single_move["description"];
+                $move_category = $single_move["category"];
+                
+                // ---------------------------------------------------------------------------------------------------------------------------------------
+                // optional params in body, checking if they are set and using null if they are not
+                $move_power = isset($single_move["power"]) ? $single_move["power"] : null;
+                $move_accuracy = isset($single_move["accuracy"]) ? $single_move["accuracy"] : null;
+                
+                $move_power_points = $single_move["power_points"];
+                $move_type = $single_move["type"];
+                
+                // turning boolean value into 1 or 0
+                $move_secondary_effect = ($single_move["has_secondary_effect"]) ? 1 : 0;
 
-            $move_record = array(
-                "name" => $move_name, 
-                "description" => $move_desc, 
-                "category" => $move_category, 
-                "power" => $move_power, 
-                "accuracy" => $move_accuracy, 
-                "power_points" => $move_power_points, 
-                "type" => $move_type, 
-                "has_secondary_effect" => $move_secondary_effect
-            );
-            $moves_model->updateMove($move_record,array( "move_id" => $move_id));
+                $move_record = array(
+                    "name" => $move_name, 
+                    "description" => $move_desc, 
+                    "category" => $move_category, 
+                    "power" => $move_power, 
+                    "accuracy" => $move_accuracy, 
+                    "power_points" => $move_power_points, 
+                    "type" => $move_type, 
+                    "has_secondary_effect" => $move_secondary_effect
+                );
+                $moves_model->updateMove($move_record,array( "move_id" => $move_id));
 
-            // preparing response message
-            $moves .= ((empty($moves)) ? "Updated rows for " . $move_name : ", " . $move_name);
+                // ---------------------------------------------------------------------------------------------------------------------------------------
+                // preparing response message
+                array_push($valid_rows, $move_record);
+            }
+            // ---------------------------------------------------------------------------------------------------------------------------------------
+            // coutning rows that were not added/invalid
+            else {
+                $rows_not_added++;
+            }
         }
         
-        $response_data = json_encode(array("message" => $moves, 
-                "moves" => $parsed_body), JSON_INVALID_UTF8_SUBSTITUTE);
+        // ---------------------------------------------------------------------------------------------------------------------------------------
+        // new reponse to include information about moves that were invalid and so not added
+        $response_data = json_encode(array("message" => 
+                count($valid_rows) . ((count($valid_rows) == 1) ? " row" : " rows") . " updated, " .
+                $rows_not_added . (($rows_not_added == 1) ? " row" : " rows") . " invalid.",
+                "moves" => $valid_rows), JSON_INVALID_UTF8_SUBSTITUTE);
     }
     else {
         $response_data = json_encode(getErrorUnsupportedFormat());
         $response_code = HTTP_UNSUPPORTED_MEDIA_TYPE;
+    }
+    
+    // ---------------------------------------------------------------------------------------------------------------------------------------
+    // change response code if all of the rows were invalid, so nothing was added to the db
+    if (empty($valid_rows) && $rows_not_added > 0) {
+        $response_code = HTTP_BAD_REQUEST;
     }
     
     $response->getBody()->write($response_data);
@@ -274,8 +318,6 @@ function handleGetMovesByPokemon(Request $request, Response $response, array $ar
 }
 
 function validateMove($single_move) {
-    $move_model = new MovesModel();
-    
     if (isset($single_move["power"])) {
         if (!(is_numeric($single_move["power"]) && $single_move["power"] >= 0)) {
             return false;
@@ -288,19 +330,15 @@ function validateMove($single_move) {
         }
     }
     
-    if (isset($single_move["name"]) && !$move_model->getMoveByName($single_move["name"])) {
-        return isset($single_move["name"]) && 
-                isset($single_move["description"]) &&
-                isset($single_move["category"]) &&
-                in_array($single_move["category"], MOVE_CATEGORIES) &&
-                isset($single_move["power_points"]) &&
-                is_numeric($single_move["power_points"]) &&
-                $single_move["power_points"] % 5 == 0 &&
-                isset($single_move["type"]) &&
-                in_array($single_move["type"], POKEMON_TYPES) &&
-                isset($single_move["has_secondary_effect"]) &&
-                ($single_move["has_secondary_effect"] === 1 || $single_move["has_secondary_effect"] === 0);
-    }
-    
-    return false;
+    return isset($single_move["name"]) && 
+            isset($single_move["description"]) &&
+            isset($single_move["category"]) &&
+            in_array($single_move["category"], MOVE_CATEGORIES) &&
+            isset($single_move["power_points"]) &&
+            is_numeric($single_move["power_points"]) &&
+            $single_move["power_points"] % 5 == 0 &&
+            isset($single_move["type"]) &&
+            in_array($single_move["type"], POKEMON_TYPES) &&
+            isset($single_move["has_secondary_effect"]) &&
+            ($single_move["has_secondary_effect"] === 1 || $single_move["has_secondary_effect"] === 0);
 }
